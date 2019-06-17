20fed2902d1d48ef75336374224edeb536b30f15
Hashem Khalifa <hashem.khalifa@gmail.com>
2019-06-18 00:20:08 +0200

    fix default value for select when switcing between the currency

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { SelectCurrency } from "./styles";
 
 export const Select = ({ value, onChange, currencyList = [] }) => (
-  <SelectCurrency defaultValue={value} onChange={onChange} name="currencyList">
+  <SelectCurrency value={value} onChange={onChange} name="currencyList">
     {currencyList.map(currency => (
       <option key={currency.id} value={currency.id}>
         {currency.currencyName}
